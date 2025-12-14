fb6dbccb0de927a8971f234f75697cd3b9c20ea2
naktan02 <jmgjmg102@gmail.com>
2025-12-14 22:58:56 +0900

    메시지 라우터 정리

--- server/src/domains/message/message.routes.js
+++ server/src/domains/message/message.routes.js
@@ -5,49 +5,21 @@ const messageController = require('./message.controller');
 const { auth, requireRole } = require('../../common/middlewares');
 
 // 공지사항 작성
-router.post(
-    '/notices',
-    auth,
-    requireRole('ADMIN'),
-    messageController.createNotice
-);
+router.post('/notices', auth, requireRole('ADMIN'), messageController.createNotice);
 
 // 공지사항 조회
-router.get(
-    '/notices',
-    auth,
-    messageController.getNotices
-);
-
+router.get('/notices', auth, messageController.getNotices);
 
 // 임시 배정 메시지 발송
-router.post(
-    '/send/temporary',
-    auth,
-    requireRole('ADMIN'),
-    messageController.sendTemporaryMessages
-);
+router.post('/send/temporary', auth, requireRole('ADMIN'), messageController.sendTemporaryMessages);
 
 // 확정 배정 메시지 발송
-router.post(
-    '/send/confirmed',
-    auth,
-    requireRole('ADMIN'),
-    messageController.sendConfirmed
-);
+router.post('/send/confirmed', auth, requireRole('ADMIN'), messageController.sendConfirmedMessages);
 
 // 내 메시지함 조회
-router.get(
-    '/',
-    auth,
-    messageController.getMyMessages
-);
+router.get('/', auth, messageController.getMyMessages);
 
 // 메시지 읽음 처리
-router.patch(
-    '/:messageId/read',
-    auth,
-    messageController.readMessage
-);
+router.patch('/:messageId/read', auth, messageController.readMessage);
 
 module.exports = router;
