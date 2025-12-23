534170204a71056f22d2058dd5611d63a7000007
naktan02 <jmgjmg102@gmail.com>
2025-12-23 21:38:30 +0900

    dist 폴더를 git에서 제거

--- server/dist/common/constants/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PROMOTION_CRITERIA = void 0;
-// server/src/common/constants/constants.ts
-exports.PROMOTION_CRITERIA = {
-    MIN_LECTURE_HOURS: 50,
-};
